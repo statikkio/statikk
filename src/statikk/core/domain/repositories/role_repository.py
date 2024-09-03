b7b10be2924ebb8259415e15b8b2373099b18a60
Adib Grouz <contact@adib-grouz.com>
2024-09-03 19:36:44 +0200

    add role repository

---
+++ src/statikk/core/domain/repositories/role_repository.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+from statikk.core.domain.entities.role import Role
+from statikk.core.domain.value_objects.role_id import RoleID
+
+class RoleRepository(ABC):
+    """
+    Interface for the Role repository that defines methods to interact with Role data.
+    """
+
+    @abstractmethod
+    def get_by_id(self, role_id: RoleID) -> Role:
+        pass
+
+    @abstractmethod
+    def save(self, role: Role) -> None:
+        pass
+
+    @abstractmethod
+    def update(self, role: Role) -> None:
+        pass
+
+    @abstractmethod
+    def delete(self, role_id: RoleID) -> None:
+        pass
